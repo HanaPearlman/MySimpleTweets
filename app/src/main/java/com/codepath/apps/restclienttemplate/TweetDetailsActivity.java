418dcec64f2fadb427374fce7efc10b9a5d10cea
Hana Pearlman <hanapearlman@fb.com>
2017-07-05 14:14:10 -0700

    Add menu to each activity with compose action

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailsActivity.java
@@ -1,30 +1,32 @@
     package com.codepath.apps.restclienttemplate;
 
     import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+    import android.content.DialogInterface;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.support.v7.app.AlertDialog;
+    import android.support.v7.app.AppCompatActivity;
+    import android.text.Editable;
+    import android.text.TextWatcher;
+    import android.util.Log;
+    import android.view.LayoutInflater;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.EditText;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.codepath.apps.restclienttemplate.models.Tweet;
-import com.loopj.android.http.JsonHttpResponseHandler;
+    import com.bumptech.glide.Glide;
+    import com.codepath.apps.restclienttemplate.models.Tweet;
+    import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONObject;
-import org.parceler.Parcels;
+    import org.json.JSONObject;
+    import org.parceler.Parcels;
 
-import cz.msebera.android.httpclient.Header;
-import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+    import cz.msebera.android.httpclient.Header;
+    import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
     public class TweetDetailsActivity extends AppCompatActivity {
 
@@ -44,6 +46,8 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
         TextView userName = (TextView) findViewById(R.id.tvUserName);
         TextView screenName = (TextView) findViewById(R.id.tvScreenName);
         TextView tweetBody = (TextView) findViewById(R.id.tvBody);
+        TextView tvFaveCount = (TextView) findViewById(R.id.tvFaveCount);
+        TextView tvRetweetCount = (TextView) findViewById(R.id.tvRetweetCount);
         ImageView profileImage = (ImageView) findViewById(R.id.ivProfileImage);
 
         ImageView retweet = (ImageView) findViewById(R.id.ibRetweet);
@@ -73,6 +77,8 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
         userName.setText(tweet.user.name);
         screenName.setText(tweet.user.screenName);
         tweetBody.setText(tweet.body);
+        tvFaveCount.setText("" + tweet.faveCount);
+        tvRetweetCount.setText("" + tweet.retweetCount);
 
         Glide.with(this)
                 .load(tweet.user.profileImageUrl)
@@ -83,6 +89,13 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
         setReplyOnClickListener();
         setFavoriteOnClickListener();
         setRetweetOnClickListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_timeline, menu);
+        return true;
     }
 
     public void setReplyOnClickListener() {
@@ -134,8 +147,8 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
                                     @Override
                                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                                         Toast.makeText(context, "Reply sent", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(context, TimeLineActivity.class);
-                                        context.startActivity(intent);
+                                        //Intent intent = new Intent(context, TimeLineActivity.class);
+                                        //context.startActivity(intent);
                                     }
 
                                     @Override
@@ -177,7 +190,8 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
                             Toast.makeText(context, "Unfavorited", Toast.LENGTH_SHORT).show();
                         }
 
-                        Intent intent = new Intent(context, TimeLineActivity.class);
+                        Intent intent = new Intent(context, TweetDetailsActivity.class);
+                        intent.putExtra("tweet", Parcels.wrap(tweet));
                         context.startActivity(intent);
                     }
 
@@ -204,7 +218,8 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
                         //mark tweet as retweeted?
                         tweet.retweeted = true;
                         Toast.makeText(context, "Retweeted", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(context, TimeLineActivity.class);
+                        Intent intent = new Intent(context, TweetDetailsActivity.class);
+                        intent.putExtra("tweet", Parcels.wrap(tweet));
                         context.startActivity(intent);
                     }
 
@@ -216,5 +231,74 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
                 });
             }
         });
+    }
+
+
+    public void onHomeTimeline(MenuItem item) {
+        //launch profile view
+        Intent intent = new Intent(this, TimeLineActivity.class);
+        startActivity(intent);
+    }
+
+    public void onComposeAction(MenuItem mi) {
+        final Context context = this;
+        View messageView = LayoutInflater.from(this).
+                inflate(R.layout.compose_modal, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(messageView);
+
+        // Create alert dialog
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        final EditText etName = (EditText) messageView.findViewById(R.id.etTweet);
+        final TwitterClient client = TwitterApp.getRestClient();
+        final TextView tvCharCount = (TextView) messageView.findViewById(R.id.tvCharCount);
+
+        final TextView tvReplyTo = (TextView) messageView.findViewById(R.id.tvReplyTo);
+        tvReplyTo.setVisibility(View.GONE);
+
+
+        TextWatcher mTextEditorWatcher = new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvCharCount.setText(String.valueOf(140 - s.length()));
+            }
+
+            public void afterTextChanged(Editable s) {}
+        };
+        etName.addTextChangedListener(mTextEditorWatcher);
+
+        // Configure dialog button (OK)
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Tweet",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        client.sendTweet(etName.getText().toString(), new JsonHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                Toast.makeText(context, "Tweet sent", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(context, TimeLineActivity.class);
+                                context.startActivity(intent);
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                                Toast.makeText(context, "Tweet failed", Toast.LENGTH_SHORT).show();
+                                Log.e("ComposeTweet onFailure", "Failure tweeting", throwable);
+                            }
+                        });
+                    }
+                });
+
+        ImageView ivCancel = (ImageView) messageView.findViewById(R.id.ivCancel);
+        ivCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.cancel();
+            }
+        });
+
+        // Display the dialog
+        alertDialog.show();
     }
 }
